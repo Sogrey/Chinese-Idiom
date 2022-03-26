813871cd19bf3ae152b7dbabe570653302a6af82
sogrey <suzhaoheng2006@163.com>
2022-03-27 02:37:00 +0800

    💖 add app bg.

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    <div id="app-bg"> </div>
+    <div id="app-bg">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
     <!-- <ChatList>
     </ChatList> -->
     <AndroidPhone id="phone-view"></AndroidPhone>
@@ -39,25 +44,81 @@ export default {
 html,
 body {
   margin: 0;
-  padding: 0;
+  padding: 0;  width: 100%;
+  height: 100%;
   overflow: hidden;
+  position: absolute;
 }
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: #2c3e50;  width: 100%;
+  height: 100%;
+  position: absolute;
 }
 #app-bg {
-  position: absolute;
+  position: relative;
   width: 100%;
   height: 100%;
   background: blueviolet;
+  display: flex;
+}
+#app-bg > div {
+  position: absolute;width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+}
+#app-bg > div:nth-child(1) {
+  background: linear-gradient(
+    238deg,
+    rgb(130, 255, 0) 0%,
+    rgb(0, 84, 166) 100%
+  );
+  animation: move 12s infinite linear;
+}
+
+#app-bg > div:nth-child(2) {
   background: linear-gradient(238deg, rgb(33, 200, 69) 0%, rgb(0, 0, 100) 100%);
   mix-blend-mode: overlay;
+  animation: move 60s infinite linear;
+}
+#app-bg > div:nth-child(3) {
+  background: radial-gradient(
+    100% 85% at 0% 100%,
+    rgb(0, 0, 0) 0%,
+    rgb(0, 131, 255) 50%,
+    rgb(255, 0, 0) 100%
+  );
+  mix-blend-mode: difference;
+  animation: move 60s infinite linear;
+}
+
+#app-bg > div:nth-child(4) {
+  background: radial-gradient(
+    100% 140% at 100% 0%,
+    rgb(0, 134, 62) 0%,
+    rgb(0, 239, 255) 50%,
+    rgb(0, 113, 186) 100%
+  );
+  mix-blend-mode: overlay;
+  animation: move 60s infinite linear;
+}
+
+@keyframes move {
+  0% {
+    filter: unset;
+  }
+
+  100% {
+    filter: hue-rotate(360deg);
+  }
 }
 #phone-view {
-  position: absolute;
-  margin: 100px 0 0 100px;
+  position: relative;
+  margin: -850px 0 0 100px;
 }
 </style>
